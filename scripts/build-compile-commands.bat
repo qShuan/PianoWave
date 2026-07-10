e2176cbe9b9ea968ec3c73140d6ba17cc472fe91
qShuan <pokorskidominik@gmail.com>
2026-07-10 20:22:00 +0200

    Added an additional check before copying debug.json, suppressing export-compile-commands stdout and stderr

--- scripts/build-compile-commands.bat
+++ scripts/build-compile-commands.bat
@@ -2,7 +2,18 @@
 
 pushd ..
 premake\premake5.exe --file=build.lua vs2022
-premake\premake5.exe --file=build.lua export-compile-commands
-copy /Y compile_commands\debug.json compile_commands.json
+
+echo Running action 'export-compile-commands'...
+premake\premake5.exe --file=build.lua export-compile-commands >nul 2>&1
+echo Done
+
+if exist compile_commands\debug.json (
+    echo Copying compile_commands\debug.json -^> compile_commands.json...
+    copy /Y compile_commands\debug.json compile_commands.json >nul
+    echo Done
+)
+
+echo Finished
+
 popd
 pause
